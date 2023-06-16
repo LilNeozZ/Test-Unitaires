b010aabf2ce6f62d911d93ad292c5638a34f1c94
LilNeozZ <77625632+LilNeozZ@users.noreply.github.com>
2023-06-16 11:25:55 +0200

    test

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -5,20 +5,24 @@ public class Book {
     private String author;
     private String publicationDate;
 
+    // Constructeur de la classe Book
     public Book(String title, String author, String publicationDate) {
         this.title = title;
         this.author = author;
         this.publicationDate = publicationDate;
     }
 
+    // Méthode pour obtenir le titre du livre
     public String getTitle() {
         return title;
     }
 
+    // Méthode pour obtenir l'auteur du livre
     public String getAuthor() {
         return author;
     }
 
+    // Méthode pour obtenir la date de publication du livre
     public String getPublicationDate() {
         return publicationDate;
     }
---
+++ src/test/java/com/example/BookTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class BookTest {
+    // Méthode pour fournir une source de livres pour les tests paramétrés
+    private static Stream<Book> bookProvider() {
+        return Stream.of(
+                new Book("Title 1", "Author 1", "2021-01-01"),
+                new Book("Title 2", "Author 2", "2022-02-02"),
+                new Book("Title 3", "Author 3", "2023-03-03")
+        );
+    }
+
+    // Test paramétré pour vérifier que le titre du livre n'est pas nul
+    @ParameterizedTest
+    @MethodSource("bookProvider")
+    void getTitle(Book book) {
+        assertThat(book.getTitle(), is(notNullValue()));
+    }
+
+    // Test paramétré pour vérifier que l'auteur du livre n'est pas nul
+    @ParameterizedTest
+    @MethodSource("bookProvider")
+    void getAuthor(Book book) {
+        assertThat(book.getAuthor(), is(notNullValue()));
+    }
+
+    // Test paramétré pour vérifier que la date de publication du livre n'est pas nulle
+    @ParameterizedTest
+    @MethodSource("bookProvider")
+    void getPublicationDate(Book book) {
+        assertThat(book.getPublicationDate(), is(notNullValue()));
+    }
+}
